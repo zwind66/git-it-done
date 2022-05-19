aff0899b603c4c4969b243ef12c3a319c8864387
Shuai Zuo <szuo1@vols.utk.edu>
2022-05-19 17:45:15 -0500

    search for feature repos done

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,8 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 
 var formSubmitHandler = function (event) {
     // prevent page from refreshing
@@ -21,6 +23,16 @@ var formSubmitHandler = function (event) {
         alert("Please enter a GitHub username");
     }
 };
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+}
 
 var getUserRepos = function (user) {
     // format the github api url
@@ -44,6 +56,20 @@ var getUserRepos = function (user) {
     });
 
 }
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+            displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
 
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
@@ -91,3 +117,4 @@ var displayRepos = function (repos, searchTerm) {
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
