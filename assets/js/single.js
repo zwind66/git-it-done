711f9c22f731db0268b2c20dd6ff88cefb1841c5
Shuai Zuo <szuo1@vols.utk.edu>
2022-05-19 16:54:39 -0500

    finish list issues part

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,22 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function (repo) {
     // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -21,7 +37,8 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -78,4 +95,5 @@ var displayWarning = function (repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
